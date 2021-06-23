95672911cea1c8a08fbfe32407ce3dcc9a29d84e
utrape <pablosud025@gmail.com>
2021-06-23 17:01:32 -0300

    Add lib configs

---
+++ mldictionary/__init__.py
@@ -0,0 +1,8 @@
+__author__ = 'Pablo Emidio'
+__email__ = 'p.emidiodev@gmail.com'
+__version__ = '0.0.2'
+
+from .dictionary import Dictionary
+from .english import English
+from .portuguese import Portuguese
+from .spanish import Spanish
