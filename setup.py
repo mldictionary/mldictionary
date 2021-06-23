81713f80620657b57b7bca9f02a1f06d959602d9
utrape <pablosud025@gmail.com>
2021-06-23 15:57:44 -0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name='mldictionary',
+    version='0.0.1',
+    description='A webscrapy dictionary',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/PabloEmidio/mldictionary',
+    author='Pablo Emidio',
+    author_email='p.emidiodev@gmail.com',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=['requests>=2.25.1', 'parsel>=1.6.0'],
+    python_requires='>=3.9',
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ],
+)
