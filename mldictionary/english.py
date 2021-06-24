ec30b3a7398107bd1a218c4e89da58cbcdd5ec5c
utrape <pablosud025@gmail.com>
2021-06-24 10:17:05 -0300

    Adapting to new rules from Dictionary parent class

--- mldictionary/english.py
+++ mldictionary/english.py
@@ -1,18 +1,7 @@
-import re
-from typing import List, Union
-
 from .dictionary import Dictionary
 
 class English(Dictionary):
     URL = 'https://dictionary.cambridge.org/us/dictionary/english/{}'
-    XPATH = '//div[has-class("def", "ddef_d", "db")]'
+    TARGET_TAG = 'div'
+    TARGET_ATTR = {'class': 'ddef_d'}
     LANGUAGE = 'English'
-
-    @classmethod
-    def _clean_html(cls, meanings_html: List[str])->Union[List[str], bool]:
-        def text_formatter(mean: str)->str:
-            mean = mean.replace('\n    \t                ', '').replace(':', '.')\
-                 .replace('\n        \n         ', ':  ')
-            return re.sub('<[^>]*>', '', mean)
-        return list(map(text_formatter, meanings_html)) or False
-
