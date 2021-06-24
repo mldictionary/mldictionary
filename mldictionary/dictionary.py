8b4fe24091e8519c77ba85f302b32b5bff6044d3
utrape <pablosud025@gmail.com>
2021-06-24 10:15:13 -0300

    Add class attributes

--- mldictionary/dictionary.py
+++ mldictionary/dictionary.py
@@ -7,23 +7,27 @@ from bs4 import BeautifulSoup
 
 class Dictionary:
     
+    URL: str
+    LANGUAGE: str
+    TARGET_TAG: str 
+    TARGET_ATTR: dict[str, str]
+
     def __str__(self)->str:
         return self.LANGUAGE
 
-    
+
     @classmethod
     def _search(cls, word: str)->str:
         with requests.get(cls.URL.format(word), \
                             headers={'User-Agent': 'Mozilla'}) as response:
             return response.text
-        
-        
+
+
     @classmethod
     @abstractmethod
     def _clean_html(self, meanings_html: List[str])->Union[List[str], bool]:
         ...
 
-        
     def get_meanings(self, word: str)->List[str]:
         word = unicodedata.normalize('NFD', word)
         word = re.sub('[\u0300-\u036f]', '', word)
