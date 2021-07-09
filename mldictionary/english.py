d067244f107ed54ec6d5da06278231a186067cef
Pablo Emidio <p.emidiodev@gmail.com>
2021-07-09 17:42:30 -0300

    Add English class docstring

--- mldictionary/english.py
+++ mldictionary/english.py
@@ -8,6 +8,25 @@ from .dictionary import Dictionary
 
 
 class English(Dictionary):
+    """Get word's meanings in English from cambridge.org.
+
+    ...
+
+    Attributes:
+        URL: str = 'https://dictionary.cambridge.org/us/dictionary/english/{}'
+            URL from a dictionary website replacing word's name to "{}"
+        LANGUAGE: str = 'English'
+            Dictionary's language's name
+        TARGET_TAG: str = 'div'
+            HTML tag which has the meanings
+        TARGET_ATTR: dict[str, str] = {'class': 'ddef_d'}
+            Pair attribute: value which makes TARGET_TAG unique
+
+    Methods:
+        get_meanings(self, word: str) -> List[str]:
+            return the word's meanings
+    """
+
     URL = 'https://dictionary.cambridge.org/us/dictionary/english/{}'
     TARGET_TAG = 'div'
     TARGET_ATTR = {'class': 'ddef_d'}
