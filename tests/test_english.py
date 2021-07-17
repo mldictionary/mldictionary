534d4051f81fe282d6d01773353185ca5436cd74
Pablo Emidio <p.emidiodev@gmail.com>
2021-07-17 20:18:48 -0300

    Add a test to check if there is a "\n" among meanings

--- tests/test_english.py
+++ tests/test_english.py
@@ -12,35 +12,13 @@ class TestGeneralEnglish(GeneralTests, unittest.TestCase):
         return super().setUp(English)
 
 class TestEnglish(unittest.TestCase):
+    word: str = 'word'
+    
     def setUp(self):
         self.english_dictionary = English()
 
-    def test_attribute_URL_type(self):
-        self.assertIsInstance(self.english_dictionary.URL, str)
-
-    def test_attribute_LANGUAGE_type(self):
-        self.assertIsInstance(self.english_dictionary.LANGUAGE, str)
-
-    def test_attribute_TARGET_TAG_type(self):
-        self.assertIsInstance(self.english_dictionary.TARGET_TAG, str)
-
-    def test_attribute_TARGET_ATTR_type(self):
-        self.assertIsInstance(self.english_dictionary.TARGET_ATTR, dict)
-
-    def test_method__search_status_code(self):
-        response = self.english_dictionary._search('word')
-        self.assertEqual(response.status_code, 200)
-
-    def test_method_get_meanings_return_type(self):
-        self.assertIsInstance(self.english_dictionary.get_meanings('word'), list)
-
-    def test_method_get_meanings_not_found_word_type(self):
-        not_found_meaning = self.english_dictionary.get_meanings('jsh')
-        self.assertIsInstance(not_found_meaning, list)
-        self.assertEqual(len(not_found_meaning), 0)
-
-    def test_method_get_meanings_return_content(self):
-        is_all_content_valid = all(
-            len(mean) > 0 for mean in self.english_dictionary.get_meanings('word')
+    def test_if_there_is_new_line_in_meanings(self):
+        there_is_new_line_in_meanings = any(
+            '\n' in meaning for meaning in self.english_dictionary.get_meanings(self.word)
         )
-        self.assertTrue(is_all_content_valid)
+        self.assertFalse(there_is_new_line_in_meanings)
