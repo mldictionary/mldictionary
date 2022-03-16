60f74a457de4adddf9a81ac7bf4092143c953f5f
Pablo Emidio <p.emidiodev@gmail.com>
2022-03-15 22:07:29 -0300

    Create a function to create dynamics Dictionary's subclasses

--- mldictionary/__init__.py
+++ mldictionary/__init__.py
@@ -4,6 +4,7 @@ __author__ = 'Pablo Emidio'
 __email__ = 'p.emidiodev@gmail.com'
 __version__ = '0.2.5'
 
+from .dictionary import create_dictionary
 from .dictionary import Dictionary
 from .english import English
 from .portuguese import Portuguese
